e3e7b8496cddb6982ccabef9eca498d477c14fba
David Froot <42048664+david-froot@users.noreply.github.com>
2022-01-12 10:21:24 -0600

    bug fix in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="isostream",
     packages=["isostream"],
-    version="1.0",
+    version="1.0.1",
     description="Python Client for the ISOStream API",
     author="ISOStream",
     author_email="info@isostream.io",
